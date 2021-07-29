32c7e33c5664b3aa7c0ff4fbcf39cbca25143bbc
Luca Biemann <55977279+LucaBiemann@users.noreply.github.com>
2021-07-30 00:33:24 +0200

    Update Chatbot_AnhKlass_Train.py

--- Chatbot_AnhKlass_Train.py
+++ Chatbot_AnhKlass_Train.py
@@ -5,7 +5,6 @@ from datetime import datetime
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 from sklearn.neighbors import KNeighborsClassifier
-from sklearn.svm import SVC
 from sklearn.gaussian_process import GaussianProcessClassifier
 from sklearn.gaussian_process.kernels import RBF
 from sklearn.tree import DecisionTreeClassifier
@@ -17,15 +16,13 @@ dataset = pd.read_pickle('Chatbot_AusweisNr')
 scaler = StandardScaler()
 classifiers = [
     KNeighborsClassifier(3),
-    SVC(kernel="linear", C=0.025),
-    SVC(gamma=2, C=1),
     DecisionTreeClassifier(max_depth=5),
     RandomForestClassifier(max_depth=5, n_estimators=10, max_features=1),
     AdaBoostClassifier(),
     GaussianNB(),
     QuadraticDiscriminantAnalysis(),
     MLPClassifier(hidden_layer_sizes=100, max_iter=1000, activation='logistic')]
-names = ["Nearest Neighbors", "Linear SVM", "RBF SVM",
+names = ["Nearest Neighbors",
          "Decision Tree", "Random Forest", "AdaBoost",
          "Naive Bayes", "QDA", "MLP"]
 for c, n in zip(classifiers, names):
